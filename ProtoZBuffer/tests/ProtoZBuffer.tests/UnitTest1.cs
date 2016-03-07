b940728747ca12031beda11f21b1afd16497e083
Gregory LEOCADIE <gregory.leocadie@gmail.com>
2016-03-07 09:42:51 +0100

    change the folder hierarchy

---
+++ ProtoZBuffer/tests/ProtoZBuffer.tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace ProtoZBuffer.tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
